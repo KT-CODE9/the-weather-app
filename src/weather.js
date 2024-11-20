44df3b920c51dddad2331a870370ce53f43b64e0
KT-CODE9 <mbuvhakhodi@gmail.com>
2024-11-20 14:16:01 +0200

    Update forecast API integration

--- src/weather.js
+++ src/weather.js
@@ -66,6 +66,13 @@ function searchCity(city) {
 
 searchCity("Thohoyandou");
 
+function formatDay(timestamp){
+  let date= new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "3aa7ceoceb04ta4430a8733bf6be036f";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -74,29 +81,32 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-  let forecastElement = document.querySelector("#forecast");
-
-  let days= ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  
-  days.forEach(function(day) {
-    
-    forecastHtml = forecastHtml + `
-    <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">☀️</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
         <div class="weather-forecast-temperatures">
-            <div class="weather-forecast-high-temperature">15°C</div>
-            <div class="weather-forecast-low-temperature">9°C</div>
+          <div class="weather-forecast-high-temperature">
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
+          </div>
+          <div class="weather-forecast-low-temperature">${Math.round(
+            day.temperature.minimum
+          )}º</div>
         </div>
-        </div>
-`;
-   });
+      </div>
+    `;
+    }
+  });
 
-   forecastElement.innerHTML=forecastHtml;
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 
-};
-
-displayForecast(Thohoyandou);
+displayForecast(response);
